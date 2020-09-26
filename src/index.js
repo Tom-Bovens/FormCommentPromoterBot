720fbdee2eaa9453c0abfeaac31dccc6849fa069
Tom Bovens <Tombovens2003@gmail.com>
2020-09-26 12:48:23 +0200

    Linted code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-
-
 const path = require('path');
+
 const envfile = `${process.cwd()}${path.sep}.env`;
 require('dotenv').config({
     path: envfile
 });
 const ChipChat = require('chipchat');
-const log = require('debug')('tourguide')
+const log = require('debug')('tourguide');
 const get = require('lodash/get');
+
 const incrementor = {
     // This function adds delay to the message to ensure the messages are posted in the right order
     autoDelayTime: 500,
-    increment: function (timeToIncrease = 1) {
+    increment(timeToIncrease = 1) {
         this.autoDelayTime += (timeToIncrease * 1000);
         return this.autoDelayTime;
     },
-    set: function (timeToSet = 1) {
+    set(timeToSet = 1) {
         this.autoDelayTime = (timeToSet * 1000);
         return this.autoDelayTime;
     }
 };
-log(process.env.HOST,process.env.TOKEN)
+log(process.env.HOST, process.env.TOKEN);
 const errorCatch = (error) => {
     log(error);
-}
-
+};
 
 // Create a new bot instance
 const bot = new ChipChat({
@@ -34,9 +33,8 @@ const bot = new ChipChat({
 
 // Crashes the code if no token is found
 if (!process.env.TOKEN) {
-    throw 'No token found, please define a token with export TOKEN=(Webhook token), or use an .env file.'
+    throw new Error('No token found, please define a token with export TOKEN=(Webhook token), or use an .env file.');
 }
-
 
 // Use any REST resource
 // bot.users.get(bot.auth.user).then((botUser) => {
@@ -48,18 +46,18 @@ bot.on('error', log);
 
 bot.on('user.create', async (loginUser) => {
     try {
-        const user = await bot.users.get(get(loginUser, 'data.user.id'))
-        const userId = user.id
-        const hasSeenGuide = get(user, 'meta.hasSeenCommentguide', 'false')
-        log(`User guide status: ${hasSeenGuide}`)
-        if (hasSeenGuide === "false") {
-            log("User has not seen the guide on result comments yet.")
-            if (user.role == 'guest') {
+        const user = await bot.users.get(get(loginUser, 'data.user.id'));
+        const userId = user.id;
+        const hasSeenGuide = get(user, 'meta.hasSeenCommentguide', 'false');
+        log(`User guide status: ${hasSeenGuide}`);
+        if (hasSeenGuide === 'false') {
+            log('User has not seen the guide on result comments yet.');
+            if (user.role === 'guest') {
                 if (userId) {
-                    let conversation
-                    const oldConvFinder = await bot.conversations.list({ name:'Result comments', participants:{ user: userId } })
+                    let conversation;
+                    const oldConvFinder = await bot.conversations.list({ name: 'Result comments', participants: { user: userId } });
                     if (oldConvFinder.length > 0) {
-                        conversation = oldConvFinder[0]
+                        conversation = oldConvFinder[0];
                         await bot.send(conversation.id, [
                             {
                                 text: `Hey there ${user.givenName}! Welcome to Chatshipper! I need to show you something. Tell me when you're ready!`,
@@ -68,64 +66,64 @@ bot.on('user.create', async (loginUser) => {
                                 delay: incrementor.set(3),
                                 actions: [
                                     {
-                                        type: "reply",
-                                        text: "Ok!",
-                                        payload: "userReturned"
+                                        type: 'reply',
+                                        text: 'Ok!',
+                                        payload: 'userReturned'
                                     }
                                 ]
                             }
-                        ])
+                        ]);
                     } else {
                         conversation = await bot.conversations.create(
                             { name: 'Result comments', messages: [{ text: `Hey there ${user.givenName}! I need to show you something!` }] }
-                        )
+                        );
                     }
                     await bot.send(conversation.id, {
                         type: 'command',
                         text: '/assign',
                         meta: {
-                            "users": [
+                            users: [
                                 userId
                             ]
                         }
-                    })
+                    });
                     await bot.send(conversation.id, [
                         {
-                            text: `Tell me when you're ready!`,
+                            text: 'Tell me when you\'re ready!',
                             isBackchannel: false,
                             role: 'bot',
                             delay: incrementor.set(3),
                             actions: [
                                 {
-                                    type: "reply",
-                                    text: "Ok!",
-                                    payload: "userReturned"
+                                    type: 'reply',
+                                    text: 'Ok!',
+                                    payload: 'userReturned'
                                 }
                             ]
                         }
-                    ])
+                    ]);
                 }
             } else {
-                log(`User is not a guest. Role is : ${user.role}`)
+                log(`User is not a guest. Role is : ${user.role}`);
             }
         }
-    } catch (e){
-        errorCatch(e)
+    } catch (e) {
+        errorCatch(e);
     }
 });
 
 bot.on('message.create.bot.postback.agent', async (message, conversation) => {
-    if (message.text === "userReturned") {
+    if (message.text === 'userReturned') {
         try {
             await bot.send(conversation.id, [
                 {
-                    text: `Chatshipper has a nice feature called result commenting, which can be used to leave feedback on forms your colleagues made!`,
+                    text: 'Chatshipper has a nice feature called result commenting, which can be used to leave feedback on forms your colleagues made!',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.increment(3)
                 },
                 {
-                    text: `Just a second, i think i have an example of it somewhere...`,
+                    text: 'Just a second, i think i have an example of it somewhere...',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.increment(3)
@@ -140,13 +138,12 @@ bot.on('message.create.bot.postback.agent', async (message, conversation) => {
                     text: '/leave',
                     delay: incrementor.increment(2)
                 }
-            ])
-        } catch (e){
-            errorCatch(e)
+            ]);
+        } catch (e) {
+            errorCatch(e);
         }
     }
 });
-
 
 // Start Express.js webhook server to start listening
 bot.start();
